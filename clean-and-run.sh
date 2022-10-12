c3d15958126e8efa2be5fa5c0dac20644836e6b1
Akin <akin.tanis.dev@gmail.com>
2022-10-12 03:20:35 +0200

    clean-and-run.sh script update

--- clean-and-run.sh
+++ clean-and-run.sh
@@ -7,6 +7,10 @@ docker container rm blog-db-backend-1
 
 docker image rm akin/blog-backend
 
+docker container rm blog-db-frontend-1
+
+docker image rm akin/blog-frontend
+
 # docker rm -f $(docker ps -a -q)
 
 docker volume rm $(docker volume ls -q)
